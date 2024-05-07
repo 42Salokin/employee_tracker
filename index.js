062872be4e12498625365f014e9f8c89b14bfb27
Salokin <nicholas.dpoul@gmail.com>
2024-05-06 20:00:41 -0400

    added prompts for add department, role, and employee functions

--- index.js
+++ index.js
@@ -19,40 +19,40 @@ function loadMainPrompts() {
     // TODO- Create first question user will see- "What would you like to do?"
     {
       type: 'list',
-      name: 'doList', 
+      name: 'doList',
       message: 'What would you like to do?',
       choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update Employee Role', 'Nothing'],
     },
   ])
-  .then((res) => {
-    const choice = res.doList;
-    switch (choice) {
-      case 'View All Departments':
-        viewDepartments();
-        break;
-      case 'View All Roles':
-        viewRoles();
-        break;
-      case 'View All Employees':
-        viewEmployees();
-        break;
-      case 'Add a Department':
-        addDepartment();
-        break;
-      case 'Add a Role':
-        addRole();
-        break;
-      case 'Add an Employee':
-        addEmployee();
-        break;
-      case 'Update Employee Role':
-        updateRole();
-        break;
-      default:
-        quit();
-        break;
-    }
-  });
+    .then((res) => {
+      const choice = res.doList;
+      switch (choice) {
+        case 'View All Departments':
+          viewDepartments();
+          break;
+        case 'View All Roles':
+          viewRoles();
+          break;
+        case 'View All Employees':
+          viewEmployees();
+          break;
+        case 'Add a Department':
+          addDepartment();
+          break;
+        case 'Add a Role':
+          addRole();
+          break;
+        case 'Add an Employee':
+          addEmployee();
+          break;
+        case 'Update Employee Role':
+          updateRole();
+          break;
+        default:
+          quit();
+          break;
+      }
+    });
 }
 
 // TODO- Create a function to View all deparments
@@ -72,18 +72,77 @@ function viewEmployees() {
 
 // TODO- Create a function to Add a department
 function addDepartment() {
-  console.log('What is the name of the department?');
+  prompt([
+    {
+      type: 'input',
+      name: 'department',
+      message: 'What is the name of the department?'
+    }
+  ])
+    .then((res) => {
+      const newDep = res.department;
+      console.log(newDep);
+    });
 }
 
 // TODO- Create a function to Add a role
 function addRole() {
-  console.log('What is the name of the role?');
+  prompt([
+    {
+      type: 'input',
+      name: 'roleName',
+      message: 'What is the name of the role?'
+    },
+    {
+      type: 'input',
+      name: 'salary',
+      message: "What is the role's salary?"
+    },
+    {
+      type: 'input',
+      name: 'department',
+      message: 'Which department is the role in?'
+    },
+  ])
+    .then((res) => {
+      const newRole = res.roleName;
+      const newSal = res.salary;
+      const newDep = res.department;
+      console.log(`${newRole}, making ${newSal} per year, has been added to ${newDep}`);
+    });
 }
 
 // TODO- Create a function to Add an employee
 function addEmployee() {
-  console.log('What is the name of the employee?');
-}
+  prompt([
+    {
+      type: 'input',
+      name: 'firstName',
+      message: "What is the employee's first name?"
+    },
+    {
+      type: 'input',
+      name: 'lastName',
+      message: "What is the employee's last name?"
+    },
+    {
+      type: 'input',
+      name: 'role',
+      message: "What is the employee's role?"
+    },
+    {
+      type: 'input',
+      name: 'manager',
+      message: "Who is the employee's manager?"
+    },
+  ])
+    .then((res) => {
+      const firstName = res.firstName;
+      const lastName = res.lastName;
+      const empRole = res.role;
+      const empManager = res.manager;
+      console.log(`${firstName} ${lastName} has been added in the role of ${empRole}, answering to ${empManager}`);
+    });}
 
 // TODO- Create a function to Update an employee's role
 function updateRole() {
